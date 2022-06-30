ffe83bd5c64c6532512f485caed0a4b566a28247
Pradum <pradumvishwakarma2@gmail.com>
2022-06-30 21:57:52 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.get('/',(req, res) => {
     res.send('Note API From Pradum786');
 })
 
-const PORT =process.env.Port || 5000;
+const PORT =process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URL).then(()=> {
     app.listen(PORT ,()=>{console.log("listening on port " + PORT)})
